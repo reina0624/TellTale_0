18e70bafbe2cd7542e72351073acec44f0c29bd1
Reina Nakade <reirei01013@gmail.com>
2025-02-05 18:37:45 +0900

    abc

---
+++ TellTale/ConnectViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ConnectViewController.swift
+//  TellTale
+//
+//  Created by Reina Nakade on 2025/02/05.
+//
+
+import UIKit
+
+class ConnectViewController: UIViewController,UITextFieldDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
