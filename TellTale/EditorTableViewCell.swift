c6e432d008adae3d89f82169a39746fad9414227
Reina Nakade <reirei01013@gmail.com>
2025-02-12 20:29:53 +0900

    tech2/12

---
+++ TellTale/EditorTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  EditorTableViewCell.swift
+//  TellTale
+//
+//  Created by Reina Nakade on 2025/02/12.
+//
+
+import UIKit
+
+class EditorTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
